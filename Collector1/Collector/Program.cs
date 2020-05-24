5a6f628b854a53cef45ffafbdd1045fe0f9df023
Ivan Myronenko <j.myrron@gmail.com>
2020-05-24 15:14:05 +0300

    Add files via upload

---
+++ Collector1/Collector/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collector
+{
+    class Program
+    {
+        static Model model = new Model();
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.GetEncoding("koi8-u");
+            Console.Title = "Collector";
+            Console.Write("Enter the path to the document: ");
+            model.SetPath(Console.ReadLine());
+            model.LoadDoc();
+            model.LoadTable();
+            model.ScanTable();
+            Console.ReadKey();
+        }
+    }
+}
